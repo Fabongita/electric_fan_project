e82d17e2b0a99235bab7d2d804c0c33061342973
Fabongita <nashmpiii@gmail.com>
2025-06-27 17:47:57 +0800

    fixed indentations

--- speed.py
+++ speed.py
@@ -8,20 +8,28 @@ class Speed(ABC):
     def increment(self) -> int: ...
 
 class Off(Speed):
-    def delay(self):     return 200
-    def increment(self): return   0
+    def delay(self):     
+        return 200
+    def increment(self): 
+        return   0
 
 class Low(Speed):
-    def delay(self):     return 150
-    def increment(self): return   8
+    def delay(self):     
+        return 150
+    def increment(self): 
+        return   8
 
 class Med(Speed):
-    def delay(self):     return  90
-    def increment(self): return  16
+    def delay(self):     
+        return  90
+    def increment(self): 
+        return  16
 
 class High(Speed):
-    def delay(self):     return  40
-    def increment(self): return  24
+    def delay(self):     
+        return  40
+    def increment(self): 
+        return  24
 
 # map 0..3 → strategy
 SPEED = {0: Off(), 1: Low(), 2: Med(), 3: High()}
